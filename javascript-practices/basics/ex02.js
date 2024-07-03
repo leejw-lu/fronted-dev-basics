fb91b9a59494e29e6b87270f0a83a1cd53e6eb27
leejw-lu <jiwoojenny@naver.com>
2024-07-03 15:44:06 +0900

    class: js basics (변수, 변수 스코프, 구문)

--- javascript-practices/basics/ex02.js
+++ javascript-practices/basics/ex02.js
@@ -24,8 +24,8 @@
  
 */
 console.log("=== 기본타입(undefined, number, string, boolean) ===");
-var u;
-var i=10;
+var u;  // var u = undefined; 과 동일, 자바스크립트는 선언과 정의가 구분없다.
+var i=10;   
 var s= 'hello world';
 var b= true;
 
@@ -42,9 +42,29 @@ var o2= {};
 var a2= [];
 var n= null;
 
-console.log("i2:"+ typeof(i2));
-console.log("s2:"+ typeof(s2));
-console.log("b2:"+ typeof(b2));
-console.log("o2:"+ typeof(o2));
-console.log("a2:"+ typeof(a2));
+console.log("i2:"+ typeof(i2) + ":" + (i2 instanceof(Number)));
+console.log("s2:"+ typeof(s2) + ":" + (i2 instanceof(String)));
+console.log("b2:"+ typeof(b2)+ ":" + (i2 instanceof(Boolean)));
+console.log("o2:"+ typeof(o2)+ ":" + (i2 instanceof(Object)));
+console.log("a2:"+ typeof(a2)+ ":" + (i2 instanceof(Array)));
 console.log("n:"+ typeof(n));
+
+console.log("=== 객체타입(function) ===");
+function f1(a,b) {
+    return a+b;
+}
+
+var f2 = function(a,b) {
+    return a+b;
+}
+
+var f3= new Function('a','b','return a+b;');
+
+console.log(f1(10,20), f2(10,20), f3(10,20));
+console.log("f1:" + typeof(f1));
+console.log("f2:" + typeof(f2)); 
+console.log("f3:" + typeof(f3));
+
+console.log("=== Primitive Type도 메소드를 호출할 수 있다.(유사객체) ===");
+console.log(b2.valueOf());
+console.log(b.valueOf());
