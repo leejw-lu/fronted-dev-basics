d8edb4f31edee6f7dff6e61ecb82933667230fb4
leejw-lu <jiwoojenny@naver.com>
2024-07-08 16:54:10 +0900

    class: ch08 init

---
+++ ch08/src/main/java/ch08/initializer/Ch08WebApplicationInitializer.java
@@ -0,0 +1,33 @@
+package ch08.initializer;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import ch08.config.WebConfig;
+
+public class Ch08WebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[]{};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] {WebConfig.class};
+	}
+	
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		return new Filter[] {new CharacterEncodingFilter("UTF-8")};
+	}
+	
+}
